1fbd514861f926e7e21b39120f9757fa420bfa24
Anju M <Anju@NTP-LAP-771.chn.nestgroup.net>
2022-12-07 15:36:30 +0530

    api search

--- src/app/search-employ/search-employ.component.ts
+++ src/app/search-employ/search-employ.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ApiService } from '../api.service';
 
 @Component({
   selector: 'app-search-employ',
@@ -6,5 +7,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./search-employ.component.css']
 })
 export class SearchEmployComponent {
-
+  empCode=""
+  
+constructor(private api:ApiService){}
+searchData:any=[]
+  readValue=()=>
+  {
+    let data:any={"empCode":this.empCode}
+    console.log(data)
+    this.api.searchEmploy(data).subscribe(
+      (response:any)=>
+      {
+        console.log(response)
+        if (response.length==0) {
+          alert("Invalid emp code")
+        } else {
+          this.searchData=response;
+        }
+      }
+    )
+  }
 }
